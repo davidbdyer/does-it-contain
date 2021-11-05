43dcd14016d5734a0329435e26aa0cc3db50a6b1
davidbdeath <social_davidbdyer@icloud.com>
2021-11-05 19:58:58 +0000

    fixed

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-const filterKeyRaw = document.getElementById('filter-key');
+const wordBankIn = document.getElementById('word-bank');
 const textToCheck = document.getElementById('text-to-check');
 const textOutput = document.getElementById('text-output');
+let wordBankArray = []
+let textInArray = []
 
-textChecker = (str, array) => {
-	strArray = str.value.split(' ')
-	strArray.forEach((word, index) => {
-		if (word.includes(array)) {
-			strArray[index] = strArray[index].replace(word, `<b>${word}</b>`)
-			textOutput.innerHTML = strArray.join(' ')
+
+const sleep = (ms) => {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+textChecker = (textIn, wordBank) => {
+	if (textIn.value != '') {
+		textInArray = textIn.value.split(/\W/)
+		// console.log(textInArray)
+		return textInArray
+	}
+}
+
+const setWordBank = async (wordBank) => {
+	if (wordBank.value != '') {
+		wordBankArray = wordBank.value.split(/\W/);
+		// console.log(wordBankArray)
+	}
+}
+
+const compareText = (wordBankArray, textInArray) => {
+	textInArray.forEach((word, index) => {
+		if (wordBankArray.includes(word)) {
+			textInArray[index] = `<b> ${word} </b>`
+			textOutput.innerHTML = textInArray.join(' ')
+		} else {
+			textOutput.innerHTML = textInArray.join(' ')
 		}
 	})
 }
 
-filterKeyRaw.addEventListener('keyup', () => {
-	const filterKeyArray = filterKeyRaw.value.split(' ');
-	textToCheck.addEventListener('keyup', (e) => {
-		e.code === 'Space' ? textChecker(textToCheck, filterKeyArray) : console.log('no')
-	})
+addEventListener('paste', async (event) => {
+	await sleep(500)
+	setWordBank(wordBankIn)
+	textChecker(textToCheck, wordBankArray)
+	compareText(wordBankArray, textInArray)
+})
+
+
+addEventListener('keyup', async (event) => {
+	await sleep(500)
+	setWordBank(wordBankIn)
+	textChecker(textToCheck, wordBankArray)
+	compareText(wordBankArray, textInArray)
 })
