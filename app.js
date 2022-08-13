dab4b95f7134bca20e98543bfc9a4f4f11b860a9
davidbdyer <social_davidbdyer@icloud.com>
2022-08-13 01:46:10 -0400

    Improved Design and Overall Function.

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
 const wordBankIn = document.getElementById('word-bank');
 const textToCheck = document.getElementById('text-to-check');
 const textOutput = document.getElementById('text-output');
-let wordBankArray = []
-let textInArray = []
+const checkBtn = document.getElementById('check-btn');
+const resetBtn = document.getElementById('reset-btn');
+const wordBankArray = [];
+const textInArray = [];
 
-
-const sleep = (ms) => {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const setTextInArray = (textIn, wordBank) => {
-	if (textIn.value != '') {
-		textInArray = textIn.value.split(/\W/)
-		// console.log(textInArray)
-	}
-}
-
-const setWordBankArray = async (wordBank) => {
-	if (wordBank.value != '') {
-		wordBankArray = wordBank.value.split(/\W/);
-		// console.log(wordBankArray)
-	}
-}
-
-const compareText = (wordBankArray, textInArray) => {
-	textInArray.forEach((word, index) => {
-		if (wordBankArray.includes(word)) {
-			textInArray[index] = `<b> ${word} </b>`
-			textOutput.innerHTML = textInArray.join(' ')
-		} else {
-			textOutput.innerHTML = textInArray.join(' ')
+const textInToArray = async (text, array) => {
+	if (text.value != '') {
+		let tmpArray = text.value.split(/\W/); // none word characters
+		for (word of tmpArray) {
+			if (word != '') {
+				array.push(word);
+			}
 		}
-	})
-}
+	}
+};
 
-addEventListener('paste', async (event) => {
-	await sleep(500)
-	setWordBank(wordBankIn)
-	textChecker(textToCheck, wordBankArray)
-	compareText(wordBankArray, textInArray)
-})
+// clears arrays
+const clearArrays = () => {
+	wordBankArray.length = 0;
+	textInArray.length = 0;
+	textOutput.innerHTML = '';
+};
 
+// resets page
+const resetPage = () => {
+	clearArrays();
+	wordBankIn.value = '';
+	textToCheck.value = '';
+};
 
-addEventListener('keyup', async (event) => {
-	await sleep(500)
-	setWordBankArray(wordBankIn)
-	setTextInArray(textToCheck, wordBankArray)
-	compareText(wordBankArray, textInArray)
-})
+const compareText = (wordBank, textIn) => {
+	for (word of textIn) {
+		if (wordBank.includes(word)) {
+			textOutput.innerHTML += `<span class="found"> ${word} </span>`;
+		}
+	}
+};
+
+// Run Page Functions
+
+checkBtn.addEventListener('click', async (event) => {
+	await clearArrays();
+	textInToArray(wordBankIn, wordBankArray);
+	textInToArray(textToCheck, textInArray);
+	compareText(wordBankArray, textInArray);
+});
+
+resetBtn.addEventListener('click', resetPage);
